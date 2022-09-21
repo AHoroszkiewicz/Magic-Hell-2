1e47fad5442a87b136e1ba076d8ead5b689fa968
Adam <loginadas@gmail.com>
2022-09-21 20:38:31 +0200

    spawning healing item

--- Assets/Scripts/Spawner/SpawnManager.cs
+++ Assets/Scripts/Spawner/SpawnManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] GameObject player;
+    [SerializeField] GameObject healItem;
     [SerializeField] List<WaveSystemSO> waves;
 
     private void Start()
@@ -14,43 +15,49 @@ public class SpawnManager : MonoBehaviour
 
     private IEnumerator EnemySpawn()
     {
-            for (int i = 0; i < waves.Capacity; i++)
+        for (int i = 0; i < waves.Capacity; i++)
+        {
+            for (int j = 0; j < waves[i].enemyAmount; j++)
             {
-                    for (int j = 0; j < waves[i].enemyAmount; j++)
-                    {
-                    Vector3 playerPos = player.transform.position;
-                    Vector3 enemySpawn;
-                    int randomNumber = Random.Range(1, 4);
-                    switch (randomNumber)
-                    {
-                        case 1:
-                            enemySpawn = new Vector3(-4, Random.Range(-6f, 6f), 0);
-                            enemySpawn += playerPos;
-                            break;
+                Vector3 playerPos = player.transform.position;
+                Vector3 enemySpawn;
+                int randomNumber = Random.Range(1, 4);
+                switch (randomNumber)
+                {
+                    case 1:
+                        enemySpawn = new Vector3(-4, Random.Range(-6f, 6f), 0);
+                        enemySpawn += playerPos;
+                        break;
 
-                        case 2:
-                            enemySpawn = new Vector3(4, Random.Range(-6f, 6f), 0);
-                            enemySpawn += playerPos;
-                            break;
+                    case 2:
+                        enemySpawn = new Vector3(4, Random.Range(-6f, 6f), 0);
+                        enemySpawn += playerPos;
+                        break;
 
-                        case 3:
-                            enemySpawn = new Vector3(Random.Range(-4f, 4f), -6, 0);
-                            enemySpawn += playerPos;
-                            break;
+                    case 3:
+                        enemySpawn = new Vector3(Random.Range(-4f, 4f), -6, 0);
+                        enemySpawn += playerPos;
+                        break;
 
-                        case 4:
-                            enemySpawn = new Vector3(Random.Range(-4f, 4f), 6, 0);
-                            enemySpawn += playerPos;
-                            break;
+                    case 4:
+                        enemySpawn = new Vector3(Random.Range(-4f, 4f), 6, 0);
+                        enemySpawn += playerPos;
+                        break;
 
-                        default:
-                            Debug.Log("enemy spawn error");
-                            enemySpawn = new Vector3(0, 0, 0);
-                            break;
-                    }
-                        Instantiate(waves[i].enemyPrefab, enemySpawn, Quaternion.identity).transform.parent = gameObject.transform;
-                        yield return new WaitForSeconds(60/ waves[i].enemyAmount );
-                    }
+                    default:
+                        Debug.Log("enemy spawn error");
+                        enemySpawn = new Vector3(0, 0, 0);
+                        break;
+                }
+            Instantiate(waves[i].enemyPrefab, enemySpawn, Quaternion.identity).transform.parent = gameObject.transform;
+
+            if (j == 0)
+            {
+                Instantiate(healItem, enemySpawn, Quaternion.identity);
+            }
+
+                yield return new WaitForSeconds(60/ waves[i].enemyAmount );
             }
         }
+    }
 }
