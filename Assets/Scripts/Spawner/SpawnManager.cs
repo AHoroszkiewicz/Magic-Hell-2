bf4446d040d5078b92c74c8d9c79efa3bfc00261
Adam <loginadas@gmail.com>
2022-10-06 19:43:59 +0200

    win screen

--- Assets/Scripts/Spawner/SpawnManager.cs
+++ Assets/Scripts/Spawner/SpawnManager.cs
@@ -6,6 +6,7 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField] GameObject player;
     [SerializeField] GameObject healItem;
+    [SerializeField] GameObject victoryUI;
     [SerializeField] List<WaveSystemSO> waves;
 
     private void Start()
@@ -15,48 +16,56 @@ public class SpawnManager : MonoBehaviour
 
     private IEnumerator EnemySpawn()
     {
-        for (int i = 0; i < waves.Capacity; i++)
+        for (int i = 0; i < waves.Capacity+1; i++)
         {
-            for (int j = 0; j < waves[i].enemyAmount; j++)
+            if (i<waves.Capacity)
             {
-                Vector3 playerPos = player.transform.position;
-                Vector3 enemySpawn;
-                int randomNumber = Random.Range(1, 4);
-                switch (randomNumber)
+                for (int j = 0; j < waves[i].enemyAmount; j++)
                 {
-                    case 1:
-                        enemySpawn = new Vector3(-4, Random.Range(-6f, 6f), 0);
-                        enemySpawn += playerPos;
-                        break;
+                    Vector3 playerPos = player.transform.position;
+                    Vector3 enemySpawn;
+                    int randomNumber = Random.Range(1, 4);
+                    switch (randomNumber)
+                    {
+                        case 1:
+                            enemySpawn = new Vector3(-4, Random.Range(-6f, 6f), 0);
+                            enemySpawn += playerPos;
+                            break;
 
-                    case 2:
-                        enemySpawn = new Vector3(4, Random.Range(-6f, 6f), 0);
-                        enemySpawn += playerPos;
-                        break;
+                        case 2:
+                            enemySpawn = new Vector3(4, Random.Range(-6f, 6f), 0);
+                            enemySpawn += playerPos;
+                            break;
 
-                    case 3:
-                        enemySpawn = new Vector3(Random.Range(-4f, 4f), -6, 0);
-                        enemySpawn += playerPos;
-                        break;
+                        case 3:
+                            enemySpawn = new Vector3(Random.Range(-4f, 4f), -6, 0);
+                            enemySpawn += playerPos;
+                            break;
 
-                    case 4:
-                        enemySpawn = new Vector3(Random.Range(-4f, 4f), 6, 0);
-                        enemySpawn += playerPos;
-                        break;
+                        case 4:
+                            enemySpawn = new Vector3(Random.Range(-4f, 4f), 6, 0);
+                            enemySpawn += playerPos;
+                            break;
 
-                    default:
-                        Debug.Log("enemy spawn error");
-                        enemySpawn = new Vector3(0, 0, 0);
-                        break;
+                        default:
+                            Debug.Log("enemy spawn error");
+                            enemySpawn = new Vector3(0, 0, 0);
+                            break;
+                    }
+                Instantiate(waves[i].enemyPrefab, enemySpawn, Quaternion.identity).transform.parent = gameObject.transform;
+
+                if (j == 0)
+                {
+                    Instantiate(healItem, enemySpawn, Quaternion.identity);
                 }
-            Instantiate(waves[i].enemyPrefab, enemySpawn, Quaternion.identity).transform.parent = gameObject.transform;
-
-            if (j == 0)
-            {
-                Instantiate(healItem, enemySpawn, Quaternion.identity);
+                    float wait = 60f / waves[i].enemyAmount;
+                    yield return new WaitForSeconds(wait);
+                }
             }
-                float wait = 60f / waves[i].enemyAmount;
-                yield return new WaitForSeconds(wait);
+            else
+            {
+                Time.timeScale = 0;
+                victoryUI.SetActive(true);
             }
         }
     }
